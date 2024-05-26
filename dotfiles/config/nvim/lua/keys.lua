76b376bc2f891bf94ccd3c8930741294ba5afb66
Dave Aitken <dave.aitken@gmail.com>
2024-05-26 16:39:03 +0100

    format lua files

--- dotfiles/config/nvim/lua/keys.lua
+++ dotfiles/config/nvim/lua/keys.lua
@@ -1,23 +1,23 @@
 local function init()
-	vim.api.nvim_create_user_command('W', ':w', {})
-	vim.api.nvim_create_user_command('Wq', ':wq', {})
-	vim.api.nvim_create_user_command('Wqa', ':wqa', {})
+  vim.api.nvim_create_user_command('W', ':w', {})
+  vim.api.nvim_create_user_command('Wq', ':wq', {})
+  vim.api.nvim_create_user_command('Wqa', ':wqa', {})
 
-	-- vim.api.nvim_set_keymap('n', '<LEADER>fed', ":e $MYVIMRC<CR>", { noremap = true, silent = true });
-	-- vim.api.nvim_create_user_command('EditVimrc', ':e $MYVIMRC', {})
-	vim.keymap.set('n', '<LEADER>fed', ":e $MYVIMRC<CR>", { desc = "Edit init.lua" });
-	vim.keymap.set('n', '<LEADER>feh', ":e ~/config-nix/home/default.nix<CR>", {});
-	vim.keymap.set('n', '<LEADER>fec', ":e ~/config-nix/flake.nix<CR>", {});
-	vim.keymap.set('n', '<LEADER>gs', ":Neogit<CR>", {});
-	vim.keymap.set('n', '<LEADER>sc', ":noh<CR>", {});
-	vim.keymap.set({'n', 'v'}, '<LEADER>cl', "gcc", { remap = true });
+  -- vim.api.nvim_set_keymap('n', '<LEADER>fed', ":e $MYVIMRC<CR>", { noremap = true, silent = true });
+  -- vim.api.nvim_create_user_command('EditVimrc', ':e $MYVIMRC', {})
+  vim.keymap.set('n', '<LEADER>fed', ":e $MYVIMRC<CR>", { desc = "Edit init.lua" });
+  vim.keymap.set('n', '<LEADER>feh', ":e ~/config-nix/home/default.nix<CR>", {});
+  vim.keymap.set('n', '<LEADER>fec', ":e ~/config-nix/flake.nix<CR>", {});
+  vim.keymap.set('n', '<LEADER>gs', ":Neogit<CR>", {});
+  vim.keymap.set('n', '<LEADER>sc', ":noh<CR>", {});
+  vim.keymap.set({ 'n', 'v' }, '<LEADER>cl', "gcc", { remap = true });
 
-	-- :help CTRL-W
-	vim.keymap.set('n', '<LEADER>w<S-l>', "<C-w><S-l>", {});
-	vim.keymap.set('n', '<LEADER>w<S-h>', "<C-w><S-h>", {});
-	vim.keymap.set('n', '<LEADER>wm', "<C-w>o", {});
+  -- :help CTRL-W
+  vim.keymap.set('n', '<LEADER>w<S-l>', "<C-w><S-l>", {});
+  vim.keymap.set('n', '<LEADER>w<S-h>', "<C-w><S-h>", {});
+  vim.keymap.set('n', '<LEADER>wm', "<C-w>o", {});
 
-	vim.keymap.set('n', '<LEADER>tw', function() vim.cmd('set list!') end, {});
+  vim.keymap.set('n', '<LEADER>tw', function() vim.cmd('set list!') end, {});
 end
 
 return { init = init }
--- dotfiles/config/nvim/lua/lang/lua.lua
+++ dotfiles/config/nvim/lua/lang/lua.lua
@@ -1,14 +1,14 @@
 local function init()
-	vim.api.nvim_create_autocmd('FileType', {
-		pattern = 'lua',
-		callback = function(ev)
-			vim.lsp.start({
-				name = 'lua-language-server',
-				cmd = { 'lua-language-server' },
-				root_dir = vim.fs.root(ev.buf, { '.luarc.json' }),
-			})
-		end,
-	})
+  vim.api.nvim_create_autocmd('FileType', {
+    pattern = 'lua',
+    callback = function(ev)
+      vim.lsp.start({
+        name = 'lua-language-server',
+        cmd = { 'lua-language-server' },
+        root_dir = vim.fs.root(ev.buf, { '.luarc.json' }),
+      })
+    end,
+  })
 end
 
 return { init = init }
