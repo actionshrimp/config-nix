0eab4c967d9048f606713258a16cbc085d5d47b4
Dave Aitken <dave.aitken@gmail.com>
2024-05-26 20:23:14 +0100

    feat(lsp/nix): wire up completions from flake outputs

--- dotfiles/config/nvim/lua/lang/nix.lua
+++ dotfiles/config/nvim/lua/lang/nix.lua
@@ -6,6 +6,24 @@ local function init()
         name = 'nixd',
         cmd = { 'nixd' },
         root_dir = vim.fs.root(ev.buf, { 'flake.nix' }),
+        settings = {
+          nixd = {
+            nixpkgs = {
+              expr = "import <nixpkgs> { }",
+            },
+            options = {
+              nixos = {
+                expr = '(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations."baracus-hyperv".options',
+              },
+              nix_darwin = {
+                expr = '(builtins.getFlake ("git+file://" + toString ./.)).darwinConfigurations."marco".options',
+              },
+              home_manager = {
+                expr = '(builtins.getFlake ("git+file://" + toString ./.)).homeConfigurations."marco".options',
+              },
+            },
+          },
+        },
       })
     end,
   })
