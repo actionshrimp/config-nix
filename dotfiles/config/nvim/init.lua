c87e467407dbf3824a5b0cdab4af336c1988430f
Dave Aitken <dave.aitken@gmail.com>
2024-05-26 17:08:37 +0100

    extract tree

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -73,41 +73,7 @@ local lazy_setup = ({
       })
     end,
   },
-  {
-    "nvim-tree/nvim-tree.lua",
-    config = function()
-      require("nvim-tree").setup({
-        actions = {
-          open_file = {
-            window_picker = {
-              enable = true,
-              picker = require('window-picker').pick_window,
-            }
-          }
-        }
-      })
-      vim.keymap.set('n', '<LEADER>pt', ":NvimTreeToggle<CR>", {});
-    end,
-  },
-  -- {
-  -- 	"nvim-neo-tree/neo-tree.nvim",
-  -- 	branch = "v3.x",
-  -- 	dependencies = {
-  -- 		"nvim-lua/plenary.nvim",
-  -- 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-  -- 		"MunifTanjim/nui.nvim",
-  -- 		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-  -- 	},
-  -- 	config = function()
-  -- 		require('neo-tree').setup({
-  -- 			window = {
-  -- 				mappings = {
-  -- 					["<tab>"] = "open",
-  -- 				}
-  -- 			}
-  -- 		})
-  -- 	end
-  -- },
+  require('tree').plugins(),
   { 'tpope/vim-unimpaired' },
   {
     "kylechui/nvim-surround",
