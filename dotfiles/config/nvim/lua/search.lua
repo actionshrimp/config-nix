68e771b3e138128ad9f713d11f9ceaca72f3501b
Dave Aitken <dave.aitken@gmail.com>
2024-05-26 16:26:49 +0100

    better direnv setup via telescope project switcher

--- dotfiles/config/nvim/lua/search.lua
+++ dotfiles/config/nvim/lua/search.lua
@@ -8,7 +8,15 @@ local function plugins()
 		},
 		config = function()
 			require 'telescope'.load_extension('project')
+			local project_actions = require("telescope._extensions.project.actions")
 			require 'telescope'.setup({
+				extensions = {
+					on_project_selected = function(prompt_bufnr)
+						-- Do anything you want in here. For example:
+						project_actions.change_working_directory(prompt_bufnr, false)
+						require("harpoon.ui").nav_file(1)
+					end
+				}
 			});
 		end
 
