6aa494627a4ee94bb1f7ce193742f4393124f121
Dave Aitken <dave.aitken@gmail.com>
2024-05-27 09:36:46 +0100

    improved nvim lua lsp setup

--- dotfiles/config/nvim/lua/lang/lua.lua
+++ dotfiles/config/nvim/lua/lang/lua.lua
@@ -1,13 +1,29 @@
+--local function init()
+--  vim.api.nvim_create_autocmd('FileType', {
+--    pattern = 'lua',
+--    callback = function(ev)
+--      vim.lsp.start({
+--        name = 'lua-language-server',
+--        cmd = { 'lua-language-server' },
+--        root_dir = vim.fs.root(ev.buf, { '.luarc.json' }),
+--      })
+--    end,
+--  })
+--end
+
 local function init()
-  vim.api.nvim_create_autocmd('FileType', {
-    pattern = 'lua',
-    callback = function(ev)
-      vim.lsp.start({
-        name = 'lua-language-server',
-        cmd = { 'lua-language-server' },
-        root_dir = vim.fs.root(ev.buf, { '.luarc.json' }),
-      })
-    end,
+  -- then setup your lsp server as usual
+  local lspconfig = require('lspconfig')
+
+  -- example to setup lua_ls and enable call snippets
+  lspconfig.lua_ls.setup({
+    settings = {
+      Lua = {
+        completion = {
+          callSnippet = "Replace"
+        }
+      }
+    }
   })
 end
 
