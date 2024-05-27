1cc1bfa80d73784d5ffd957238f0ed11bfe6b670
Dave Aitken <dave.aitken@gmail.com>
2024-05-27 16:34:37 +0100

    update nvim-tree when switching project/file

--- dotfiles/config/nvim/lua/tree.lua
+++ dotfiles/config/nvim/lua/tree.lua
@@ -3,6 +3,12 @@ local function plugins()
     "nvim-tree/nvim-tree.lua",
     config = function()
       require("nvim-tree").setup({
+        sync_root_with_cwd = true,
+        respect_buf_cwd = true,
+        update_focused_file = {
+          enable = true,
+          update_root = true
+        },
         actions = {
           open_file = {
             window_picker = {
