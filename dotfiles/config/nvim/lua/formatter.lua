ddfa4be80c937390cb691d2000fe082e48e148a9
Dave Aitken <dave.aitken@gmail.com>
2024-05-30 21:12:55 +0100

    format on save

--- dotfiles/config/nvim/lua/formatter.lua
+++ dotfiles/config/nvim/lua/formatter.lua
@@ -1,9 +1,18 @@
 local M = {}
-M.plugins = function ()
-  return { { 'stevearc/conform.nvim', opts = {}, } }
+M.plugins = function()
+  return { {
+    'stevearc/conform.nvim',
+    opts = {
+      format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+    },
+  } }
 end
 
-M.init = function ()
+M.init = function()
   vim.keymap.set('n', '<LEADER>m=b', vim.lsp.buf.format);
 end
 return M
