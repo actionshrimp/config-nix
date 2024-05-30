ee547b33d36dac672f6d9fa750dd37ebae247491
Dave Aitken <dave.aitken@gmail.com>
2024-05-30 21:13:08 +0100

    json commentstring

--- dotfiles/config/nvim/lua/lang/json.lua
+++ dotfiles/config/nvim/lua/lang/json.lua
@@ -3,6 +3,7 @@ M.init = function()
   vim.api.nvim_create_autocmd('FileType', {
     pattern = 'json',
     callback = function(ev)
+      vim.cmd.set("commentstring=//\\ %s")
       vim.lsp.start({
         name = 'vscode-json-languageserver',
         cmd = { 'vscode-json-languageserver', "--stdio" },
