74b2c2106b369ae49fe6a1dc3f7600dab8209bf3
Dave Aitken <dave.aitken@gmail.com>
2024-06-01 19:27:12 +0100

    noice

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -49,6 +49,21 @@ local lazy_spec = ({
   },
   --{ "folke/neoconf.nvim", cmd = "Neoconf" },
   { "neovim/nvim-lspconfig" },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    }
+  },
   { "folke/neodev.nvim",    opts = {}, config = function() require('neodev').setup({}) end },
   {
     'alexghergh/nvim-tmux-navigation',
