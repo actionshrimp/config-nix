4c03660efb09e7f102029aa91322b674fa0d3f97
Dave Aitken <dave.aitken@gmail.com>
2024-06-01 08:47:45 +0100

    tidy: theme2

---
+++ dotfiles/config/nvim/lua/theme.lua
@@ -0,0 +1,21 @@
+local M = {}
+M.plugins = function()
+  return {
+    {
+      "rebelot/kanagawa.nvim",
+      config = function()
+        vim.cmd.colorscheme('kanagawa')
+      end
+    },
+    -- {
+    --   "folke/tokyonight.nvim",
+    --   lazy = false,
+    --   priority = 1000,
+    --   opts = {},
+    --   config = function()
+    --     vim.cmd.colorscheme('tokyonight')
+    --   end
+    -- }
+  }
+end
+return M
