9e4104e818134d77ead45c888acb97dd7be64688
Dave Aitken <dave.aitken@gmail.com>
2024-06-02 19:56:54 +0100

    nvim: load theme early

--- dotfiles/config/nvim/lua/theme.lua
+++ dotfiles/config/nvim/lua/theme.lua
@@ -3,6 +3,8 @@ M.plugins = function()
   return {
     {
       "rebelot/kanagawa.nvim",
+      lazy = false,
+      priority = 1000,
       config = function()
         vim.cmd.colorscheme("kanagawa")
       end,
