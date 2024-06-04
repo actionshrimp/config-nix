e1f40b1856e9a68a1f3a5ffb3174a429e9a1fd7a
Dave Aitken <dave.aitken@gmail.com>
2024-06-04 13:22:55 +0100

    description for key binds

--- dotfiles/config/nvim/lua/keys.lua
+++ dotfiles/config/nvim/lua/keys.lua
@@ -8,24 +8,29 @@ M.init = function()
   -- vim.api.nvim_set_keymap('n', '<LEADER>fed', ":e $MYVIMRC<CR>", { noremap = true, silent = true });
   -- vim.api.nvim_create_user_command('EditVimrc', ':e $MYVIMRC', {})
   vim.keymap.set("n", "<LEADER>fed", ":e $MYVIMRC<CR>", { desc = "Edit init.lua" })
-  vim.keymap.set("n", "<LEADER>feh", ":e ~/config-nix/home/default.nix<CR>", {})
-  vim.keymap.set("n", "<LEADER>fec", ":e ~/config-nix/flake.nix<CR>", {})
-  vim.keymap.set("n", "<LEADER>fek", ":e ~/config-nix/dotfiles/config/nvim/lua/keys.lua<CR>", {})
+  vim.keymap.set("n", "<LEADER>feh", ":e ~/config-nix/home/default.nix<CR>", { desc = "Edit home/default.nix" })
+  vim.keymap.set("n", "<LEADER>fec", ":e ~/config-nix/flake.nix<CR>", { desc = "Edit flake.nix" })
+  vim.keymap.set(
+    "n",
+    "<LEADER>fek",
+    ":e ~/config-nix/dotfiles/config/nvim/lua/keys.lua<CR>",
+    { desc = "Edit keys.lua" }
+  )
   vim.keymap.set("n", "<LEADER>fyy", function()
     local f = vim.fn.expand("%:p")
     vim.fn.setreg("+", f)
     print(f)
-  end, {})
+  end, { desc = "Copy current filename" })
 
   vim.keymap.set("n", "<LEADER>bd", "<CMD>bd<CR>")
 
   -- commenting
-  vim.keymap.set("n", "<LEADER>cl", "gcc", { remap = true })
-  vim.keymap.set("v", "<LEADER>cl", "gc", { remap = true })
+  vim.keymap.set("n", "<LEADER>cl", "gcc", { remap = true, desc = "Comment line" })
+  vim.keymap.set("v", "<LEADER>cl", "gc", { remap = true, desc = "Comment selection" })
 
   -- system clipboard
-  vim.keymap.set("v", "<LEADER>xy", '"+y', { remap = true })
-  vim.keymap.set("n", "<LEADER>xp", '"+p', { remap = true })
+  vim.keymap.set("v", "<LEADER>xy", '"+y', { remap = true, desc = "Clipboard yank" })
+  vim.keymap.set("n", "<LEADER>xp", '"+p', { remap = true, desc = "Clipboard paste" })
 
   -- :help CTRL-W
   vim.keymap.set("n", "<LEADER>w<S-l>", "<C-w><S-l>", {})
@@ -34,15 +39,15 @@ M.init = function()
 
   vim.keymap.set("n", "<LEADER>tw", function()
     vim.cmd("set list!")
-  end, {})
+  end, { desc = "Toggle whitespace" })
   vim.keymap.set("n", "<LEADER>tl", function()
     vim.cmd("set wrap!")
-  end, {})
+  end, { desc = "Toggle linewrap" })
 
-  vim.keymap.set("n", "<LEADER>mrr", vim.lsp.buf.rename)
-  vim.keymap.set("n", "<LEADER>mgg", vim.lsp.buf.definition)
-  vim.keymap.set("n", "<LEADER>mgt", vim.lsp.buf.type_definition)
-  vim.keymap.set("n", "<LEADER>mgb", "<C-o>", {})
-  vim.keymap.set("n", "<LEADER>maa", vim.lsp.buf.code_action)
+  vim.keymap.set("n", "<LEADER>mR", vim.lsp.buf.rename, { desc = "LSP Rename" })
+  vim.keymap.set("n", "<LEADER>mgg", vim.lsp.buf.definition, { desc = "LSP definition" })
+  vim.keymap.set("n", "<LEADER>mgt", vim.lsp.buf.type_definition, { desc = "LSP type definition" })
+  vim.keymap.set("n", "<LEADER>mgb", "<C-o>", { desc = "Go back" })
+  vim.keymap.set("n", "<LEADER>maa", vim.lsp.buf.code_action, { desc = "LSP Code action" })
 end
 return M
