867aa445948a8e252398d08327964a9956e46465
Dave Aitken <dave.aitken@gmail.com>
2024-06-11 19:44:48 +0100

    switch ocamllsp to lspconfig's setup

--- dotfiles/config/nvim/lua/lang/ocaml.lua
+++ dotfiles/config/nvim/lua/lang/ocaml.lua
@@ -1,16 +1,7 @@
 local M = {}
 M.init = function()
   -- :lua print(vim.bo.filetype)
-  vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "ocaml" },
-    callback = function(ev)
-      vim.lsp.start({
-        name = "ocaml-lsp-server",
-        cmd = { "ocamllsp" },
-        root_dir = vim.fs.root(ev.buf, { "dune-project" }),
-      })
-    end,
-  })
+  require("lspconfig").ocamllsp.setup({})
 end
 
 M.plugins = function()
