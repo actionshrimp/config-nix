cecbd32a5f253910dac8a18e3881e1a88e338e72
Dave Aitken <dave.aitken@gmail.com>
2024-06-12 19:49:50 +0100

    use ocamllsp lspconfig setup for reason too

--- dotfiles/config/nvim/lua/lang/ocaml.lua
+++ dotfiles/config/nvim/lua/lang/ocaml.lua
@@ -9,15 +9,15 @@ M.plugins = function()
     {
       "reasonml-editor/vim-reason-plus",
       config = function()
+        vim.cmd.set("commentstring=//\\ %s")
         vim.api.nvim_create_autocmd("FileType", {
           pattern = { "reason" },
           callback = function(ev)
-            vim.cmd.set("commentstring=//\\ %s")
-            vim.lsp.start({
-              name = "ocaml-lsp-server",
-              cmd = { "ocamllsp" },
-              root_dir = vim.fs.root(ev.buf, { "dune-project" }),
-            })
+            -- vim.lsp.start({
+            --   name = "ocaml-lsp-server",
+            --   cmd = { "ocamllsp" },
+            --   root_dir = vim.fs.root(ev.buf, { "dune-project" }),
+            -- })
 
             vim.lsp.start({
               name = "tailwindcss-language-server",
