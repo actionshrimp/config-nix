335e7c1f38b63eec77b9cbadf85f06ed5c5be389
Dave Aitken <dave.aitken@gmail.com>
2024-07-05 12:51:47 +0100

    add ocaml overlay nix cache

--- system/common.nix
+++ system/common.nix
@@ -19,10 +19,15 @@
     distributedBuilds = true;
     inherit buildMachines;
     package = pkgs.nixVersions.nix_2_20;
-    settings.substituters = [ "https://cache.nixos.org/" "https://nix-community.cachix.org" ];
+    settings.substituters = [
+      "https://cache.nixos.org/"
+      "https://nix-community.cachix.org"
+      "https://anmonteiro.nix-cache.workers.dev"
+    ];
     settings.trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "ocaml.nix-cache.com-1:/xI2h2+56rwFfKyyFVbkJSeGqSIYMC/Je+7XXqGKDIY="
     ];
   };
 }
