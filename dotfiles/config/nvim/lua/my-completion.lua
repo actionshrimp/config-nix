159957deb076578bfc1932ec57501c0cb2641dcb
Dave Aitken <dave.aitken@gmail.com>
2024-07-25 11:43:28 +0100

    fix: actually install cmp-buffer

--- dotfiles/config/nvim/lua/my-completion.lua
+++ dotfiles/config/nvim/lua/my-completion.lua
@@ -2,20 +2,15 @@ local M = {}
 M.plugins = function()
   return {
     { "hrsh7th/cmp-nvim-lsp" },
+    { "hrsh7th/cmp-buffer" },
     {
       "hrsh7th/nvim-cmp",
       config = function()
         local cmp = require("cmp")
         cmp.setup({
           sources = cmp.config.sources({
-            {
-              name = "nvim_lsp",
-              option = {
-                ocamllsp = {
-                  keyword_pattern = [[~\?\k\+]],
-                },
-              },
-            },
+            { name = "nvim_lsp" },
+            { name = "buffer" },
           }),
           mapping = cmp.mapping.preset.insert({
             ["<C-Space>"] = cmp.mapping.complete(),
