a96af102eb56ec3c3e84e9737449a403cfc8d41d
Dave Aitken <dave.aitken@gmail.com>
2024-07-29 09:43:46 +0100

    migrating keys file to grouped structure

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -25,6 +25,7 @@ local lazy_spec = {
     dev = true,
     config = true,
   },
+  require("keys").plugins(),
   "nvim-tree/nvim-web-devicons",
   "tpope/vim-repeat",
   {
@@ -38,19 +39,6 @@ local lazy_spec = {
     config = function()
       require("lualine").setup({})
     end,
-  },
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    init = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 300
-    end,
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    },
   },
   require("my-lsp").plugins(),
   require("my-completion").plugins(),
