c9a8aa9c8d43ccad243738655aecabcddfed9ea1
Dave Aitken <dave.aitken@gmail.com>
2024-08-25 16:59:49 +0100

    add nvim-lightbulb

--- dotfiles/config/nvim/lua/my-lsp.lua
+++ dotfiles/config/nvim/lua/my-lsp.lua
@@ -2,6 +2,20 @@ local M = {}
 M.plugins = function()
   return {
     { "neovim/nvim-lspconfig" },
+    {
+      "kosayoda/nvim-lightbulb",
+      config = function()
+        require("nvim-lightbulb").setup({
+          autocmd = { enabled = true },
+          sign = {
+            enabled = false,
+          },
+          virtual_text = {
+            enabled = true,
+          },
+        })
+      end,
+    },
   }
 end
 return M
