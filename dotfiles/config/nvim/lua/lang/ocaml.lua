000dee2be0037791d6062ffcce842790e8244d2f
Dave Aitken <dave.aitken@gmail.com>
2024-09-27 14:28:10 +0100

    fix: commentstring setup

--- dotfiles/config/nvim/lua/lang/ocaml.lua
+++ dotfiles/config/nvim/lua/lang/ocaml.lua
@@ -2,15 +2,18 @@ local M = {}
 M.init = function()
   -- :lua print(vim.bo.filetype)
   require("lspconfig").ocamllsp.setup({})
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = "reason",
+    callback = function(ev)
+      vim.cmd.set("commentstring=//\\ %s")
+    end,
+  })
 end
 
 M.plugins = function()
   return {
     {
       "reasonml-editor/vim-reason-plus",
-      config = function()
-        vim.cmd.set("commentstring=//\\ %s")
-      end,
     },
   }
 end
