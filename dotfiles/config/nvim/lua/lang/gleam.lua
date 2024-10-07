3131eb9a7cf083eb87653d872015186c0d985ca2
Dave Aitken <dave.aitken@gmail.com>
2024-10-07 09:53:49 +0100

    wip: async direnv/lsp config

--- dotfiles/config/nvim/lua/lang/gleam.lua
+++ dotfiles/config/nvim/lua/lang/gleam.lua
@@ -1,5 +1,9 @@
 local M = {}
 M.init = function()
-  require("lspconfig").gleam.setup({})
+  require("lspconfig").gleam.setup({ autostart = false })
+
+  require("direnv-nvim").on_direnv_finished({ filetype = { "gleam" } }, function()
+    vim.cmd("LspStart")
+  end)
 end
 return M
--- dotfiles/config/nvim/lua/lang/python.lua
+++ dotfiles/config/nvim/lua/lang/python.lua
@@ -1,6 +1,10 @@
 local M = {}
 M.init = function()
-  require("lspconfig").pyright.setup({})
+  require("lspconfig").pyright.setup({ autostart = false })
+
+  require("direnv-nvim").on_direnv_finished({ filetype = { "python" } }, function()
+    vim.cmd("LspStart")
+  end)
 
   local null_ls = require("null-ls")
   null_ls.setup({
