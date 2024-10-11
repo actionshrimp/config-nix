d5bdbf927b979fbdbfbb3cd24bf910feaac1031f
Dave Aitken <dave.aitken@gmail.com>
2024-10-11 09:27:41 +0100

    ensure treesitter for go

--- dotfiles/config/nvim/lua/my-treesitter.lua
+++ dotfiles/config/nvim/lua/my-treesitter.lua
@@ -10,7 +10,7 @@ M.plugins = function()
       dependencies = { "nvim-treesitter/nvim-treesitter" },
       config = function()
         require("nvim-treesitter.configs").setup({
-          ensure_installed = { "ocaml", "vimdoc", "vim", "lua", "norg", "gleam" },
+          ensure_installed = { "ocaml", "vimdoc", "vim", "lua", "norg", "gleam", "go" },
           highlight = { enable = true },
           textobjects = {
             select = {
