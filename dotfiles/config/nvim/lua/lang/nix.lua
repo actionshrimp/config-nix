2f5e18fcabf87392ca0231cc727ca6135261de58
Dave Aitken <dave.aitken@gmail.com>
2024-10-31 14:21:33 +0000

    refactor: simplify nixd setup with lspconfig

--- dotfiles/config/nvim/lua/lang/nix.lua
+++ dotfiles/config/nvim/lua/lang/nix.lua
@@ -1,35 +1,61 @@
 local M = {}
 M.init = function()
-  vim.api.nvim_create_autocmd("FileType", {
-    pattern = "nix",
-    callback = function(ev)
-      vim.lsp.start({
-        name = "nixd",
-        cmd = { "nixd" },
-        root_dir = vim.fs.root(ev.buf, { "flake.nix" }),
-        settings = {
-          nixd = {
-            nixpkgs = {
-              -- expr = "import <nixpkgs> {}",
-              -- # note this only completes on a with pkgs; [ .. ] block
-              -- # see https://github.com/nix-community/nixd/issues/492
-              expr = 'import (builtins.getFlake ("git+file://" + toString ./.)).inputs.nixpkgs { }',
-            },
-            options = {
-              nixos = {
-                expr = '(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations."baracus-hyperv".options',
-              },
-              nix_darwin = {
-                expr = '(builtins.getFlake ("git+file://" + toString ./.)).darwinConfigurations."marco".options',
-              },
-              home_manager = {
-                expr = '(builtins.getFlake ("git+file://" + toString ./.)).homeConfigurations."marco".options',
-              },
-            },
+  require("lspconfig").nixd.setup({
+    autostart = false,
+    timeout = 2000,
+    settings = {
+      nixd = {
+        nixpkgs = {
+          -- expr = "import <nixpkgs> {}",
+          -- # note this only completes on a with pkgs; [ .. ] block
+          -- # see https://github.com/nix-community/nixd/issues/492
+          expr = 'import (builtins.getFlake ("git+file://" + toString ./.)).inputs.nixpkgs { }',
+        },
+        options = {
+          nixos = {
+            expr = '(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations."baracus-hyperv".options',
+          },
+          nix_darwin = {
+            expr = '(builtins.getFlake ("git+file://" + toString ./.)).darwinConfigurations."marco".options',
+          },
+          home_manager = {
+            expr = '(builtins.getFlake ("git+file://" + toString ./.)).homeConfigurations."marco".options',
           },
         },
-      })
-    end,
+      },
+    },
   })
+  -- vim.api.nvim_create_autocmd("FileType", {
+  --   pattern = "nix",
+  --   callback = function(ev)
+  --     vim.lsp.start({
+  --       name = "nixd",
+  --       cmd = { "nixd" },
+  --       root_dir = vim.fs.root(ev.buf, { "flake.nix" }),
+  --       settings = {
+  --         timeout = 2000,
+  --         nixd = {
+  --           nixpkgs = {
+  --             -- expr = "import <nixpkgs> {}",
+  --             -- # note this only completes on a with pkgs; [ .. ] block
+  --             -- # see https://github.com/nix-community/nixd/issues/492
+  --             expr = 'import (builtins.getFlake ("git+file://" + toString ./.)).inputs.nixpkgs { }',
+  --           },
+  --           options = {
+  --             nixos = {
+  --               expr = '(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations."baracus-hyperv".options',
+  --             },
+  --             nix_darwin = {
+  --               expr = '(builtins.getFlake ("git+file://" + toString ./.)).darwinConfigurations."marco".options',
+  --             },
+  --             home_manager = {
+  --               expr = '(builtins.getFlake ("git+file://" + toString ./.)).homeConfigurations."marco".options',
+  --             },
+  --           },
+  --         },
+  --       },
+  --     })
+  --   end,
+  -- })
 end
 return M
