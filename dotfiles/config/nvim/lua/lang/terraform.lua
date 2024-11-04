5137b8313e222e6b0e12d16730f36244a85c801d
Dave Aitken <dave.aitken@gmail.com>
2024-11-04 11:52:44 +0000

    feat(nvim): improve terraform setup

--- dotfiles/config/nvim/lua/lang/terraform.lua
+++ dotfiles/config/nvim/lua/lang/terraform.lua
@@ -1,7 +1,17 @@
 local M = {}
 M.plugins = function()
-  return {
-    { "hashivim/vim-terraform" },
-  }
+  return {}
+end
+M.init = function()
+  vim.cmd([[silent! autocmd! filetypedetect BufRead,BufNewFile *.tf]])
+  vim.cmd([[autocmd BufRead,BufNewFile *.hcl set filetype=hcl]])
+  vim.cmd([[autocmd BufRead,BufNewFile .terraformrc,terraform.rc set filetype=hcl]])
+  vim.cmd([[autocmd BufRead,BufNewFile *.tf,*.tfvars set filetype=terraform]])
+  vim.cmd([[autocmd BufRead,BufNewFile *.tfstate,*.tfstate.backup set filetype=json]])
+
+  vim.treesitter.language.register("hcl", "terraform")
+
+  require("lspconfig").terraform_lsp.setup({ autostart = false })
+  require("lspconfig").tflint.setup({ autostart = false })
 end
 return M
