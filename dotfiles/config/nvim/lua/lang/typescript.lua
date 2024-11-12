cc266635df0e43dc16d73d2b0fc0f1b960d396fc
Dave Aitken <dave.aitken@gmail.com>
2024-11-12 21:15:04 +0000

    more fine-grained lsp markers for deno vs ts_ls

--- dotfiles/config/nvim/lua/lang/typescript.lua
+++ dotfiles/config/nvim/lua/lang/typescript.lua
@@ -1,5 +1,15 @@
 local M = {}
 M.init = function()
-  require("lspconfig").denols.setup({ autostart = false })
+  local util = require("lspconfig.util")
+  require("lspconfig").denols.setup({
+    single_file_support = false, -- needed otherwise root_dir is ignored
+    root_dir = util.root_pattern({ "deno.json" }),
+    autostart = false,
+  })
+  require("lspconfig").ts_ls.setup({
+    single_file_support = false, -- needed otherwise root_dir is ignored
+    root_dir = util.root_pattern({ "tsconfig.json" }),
+    autostart = false,
+  })
 end
 return M
