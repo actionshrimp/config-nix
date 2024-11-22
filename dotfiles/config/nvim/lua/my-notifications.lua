96f405398881e2e3062f3cfa30b5ed7ca916c70e
Dave Aitken <dave.aitken@gmail.com>
2024-11-22 09:57:44 +0000

    nvim: change notification style, tidy

--- dotfiles/config/nvim/lua/my-notifications.lua
+++ dotfiles/config/nvim/lua/my-notifications.lua
@@ -1,28 +1,29 @@
 local M = {}
 M.plugins = function()
   return {
-    {
-      "echasnovski/mini.notify",
-      version = "*",
-      config = function()
-        require("mini.notify").setup({
-          -- Use notification message as is
-          format = function(notif)
-            return notif.msg
-          end,
-          -- bottom right
-          window = { config = { anchor = "SE" } },
-        })
-      end,
-    },
+    -- {
+    --   "echasnovski/mini.notify",
+    --   version = "*",
+    --   config = function()
+    --     require("mini.notify").setup({
+    --       -- Use notification message as is
+    --       format = function(notif)
+    --         return notif.msg
+    --       end,
+    --       -- bottom right
+    --       window = { config = { anchor = "SE" } },
+    --     })
+    --   end,
+    -- },
     {
       "rcarriga/nvim-notify",
       config = function()
         require("notify").setup({
           top_down = false,
           timeout = 500,
-          render = "compact",
-          stages = "fade",
+          -- render = "compact",
+          render = "default",
+          stages = "static",
         })
 
         -- set vim-notify as the default notification handler
