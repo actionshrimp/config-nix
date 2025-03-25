330e6b18ff69efdeef601053846b7af4eca280b1
Dave Aitken <dave.aitken@gmail.com>
2025-03-25 16:55:40 +0000

    black -> ruff lsp

--- dotfiles/config/nvim/lua/lang/python.lua
+++ dotfiles/config/nvim/lua/lang/python.lua
@@ -1,12 +1,6 @@
 local M = {}
 M.init = function()
   require("lspconfig").pyright.setup({ autostart = false })
-
-  local null_ls = require("null-ls")
-  null_ls.setup({
-    sources = {
-      null_ls.builtins.formatting.black,
-    },
-  })
+  require("lspconfig").ruff.setup({ autostart = false })
 end
 return M
