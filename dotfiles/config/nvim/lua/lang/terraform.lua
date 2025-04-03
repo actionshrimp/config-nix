b763a6fd861c9cf79e49e6b3886a54918cb37411
Dave Aitken <dave.aitken@gmail.com>
2025-04-03 11:45:28 +0100

    set terraform commentstring

--- dotfiles/config/nvim/lua/lang/terraform.lua
+++ dotfiles/config/nvim/lua/lang/terraform.lua
@@ -13,5 +13,12 @@ M.init = function()
 
   require("lspconfig").terraform_lsp.setup({ autostart = false })
   require("lspconfig").tflint.setup({ autostart = false })
+
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = "terraform",
+    callback = function(ev)
+      vim.cmd.set("commentstring=#\\ %s")
+    end,
+  })
 end
 return M
