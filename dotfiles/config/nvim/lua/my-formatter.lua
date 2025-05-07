06730b065baf4583b594d5268d4f799def79558a
Dave Aitken <dave.aitken@gmail.com>
2025-05-07 16:14:01 +0100

    autoformat iml with ocamlformat

--- dotfiles/config/nvim/lua/my-formatter.lua
+++ dotfiles/config/nvim/lua/my-formatter.lua
@@ -6,11 +6,20 @@ M.plugins = function()
       config = function()
         vim.keymap.set("n", "<LEADER>m=b", vim.lsp.buf.format)
         require("conform").setup({
+          async = true,
           format_on_save = {
             -- These options will be passed to conform.format()
             timeout_ms = 1000,
             lsp_fallback = true,
           },
+
+          formatters = {
+            ocamlformat_iml = {
+              command = "ocamlformat",
+              args = { "--impl", "$FILENAME" },
+            },
+          },
+
           -- :lua print(vim.bo.filetype)
           formatters_by_ft = {
             javascript = { "prettier" },
@@ -19,6 +28,8 @@ M.plugins = function()
             lua = { "stylua" },
             terraform = { "terraform_fmt" },
             nix = { "nixfmt" },
+            ocaml = { "ocamlformat" },
+            iml = { "ocamlformat_iml" },
           },
         })
       end,
