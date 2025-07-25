70b93be1444c6c5a201322d4fb16d51a0c3bd2c1
Dave Aitken <dave.aitken@gmail.com>
2025-07-25 08:38:46 +0100

    add devenv caches

--- system/common.nix
+++ system/common.nix
@@ -26,14 +26,17 @@
     distributedBuilds = true;
 
     settings.substituters = [
+      "https://devenv.cachix.org"
       "https://nix-community.cachix.org"
       "https://anmonteiro.nix-cache.workers.dev"
     ];
     settings.trusted-substituters = [
+      "https://devenv.cachix.org"
       "https://nix-community.cachix.org"
       "https://anmonteiro.nix-cache.workers.dev"
     ];
     settings.trusted-public-keys = [
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "ocaml.nix-cache.com-1:/xI2h2+56rwFfKyyFVbkJSeGqSIYMC/Je+7XXqGKDIY="
     ];
