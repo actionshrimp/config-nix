786049f912394cd0f947025a76d2c8f7a7d0596d
Dave Aitken <dave.aitken@gmail.com>
2025-08-20 10:14:54 +0100

    sourcekit-lsp path from xcode bundle

--- dotfiles/config/nvim/lua/lang/swift.lua
+++ dotfiles/config/nvim/lua/lang/swift.lua
@@ -1,5 +1,8 @@
 local M = {}
 M.init = function()
   vim.lsp.enable("sourcekit")
+  vim.lsp.config("sourcekit", {
+    cmd = { "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp" },
+  })
 end
 return M
