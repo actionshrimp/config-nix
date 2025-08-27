60954f5caca0e5b594e91d3bfe99d3a5c791f1c0
Dave Aitken <dave.aitken@gmail.com>
2025-08-27 20:16:14 +0100

    swap tmux for wezterm's native stuff

--- dotfiles/wezterm.lua
+++ dotfiles/wezterm.lua
@@ -10,7 +10,10 @@ config.audible_bell = "Disabled"
 
 -- For example, changing the color scheme:
 -- config.color_scheme = "AdventureTime"
-config.enable_tab_bar = false
+config.enable_tab_bar = true
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+
 config.font = wezterm.font_with_fallback({
   -- Main font
   "JetBrainsMono NFM",
@@ -18,6 +21,98 @@ config.font = wezterm.font_with_fallback({
 })
 -- config.front_end = "WebGpu" -- https://github.com/wezterm/wezterm/issues/6005
 config.harfbuzz_features = { "calt = 0", "clig = 0", "liga = 0" }
+
+local function is_vim(pane)
+  -- this is set by the plugin, and unset on ExitPre in Neovim
+  return pane:get_user_vars().IS_NVIM == "true"
+end
+
+local direction_keys = {
+  h = "Left",
+  j = "Down",
+  k = "Up",
+  l = "Right",
+}
+
+local function split_nav(resize_or_move, key)
+  return {
+    key = key,
+    mods = resize_or_move == "resize" and "META" or "CTRL",
+    action = wezterm.action_callback(function(win, pane)
+      if is_vim(pane) then
+        -- pass the keys through to vim/nvim
+        win:perform_action({
+          SendKey = { key = key, mods = (resize_or_move == "resize" and "META") or "CTRL" },
+        }, pane)
+      else
+        if resize_or_move == "resize" then
+          win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+        else
+          win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+        end
+      end
+    end),
+  }
+end
+
+-- Leader is the same as my old tmux prefix
+config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = {
+  -- splitting
+  {
+    mods = "LEADER",
+    key = "s",
+    action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+  },
+  {
+    mods = "LEADER",
+    key = "v",
+    action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+  },
+  {
+    mods = "LEADER",
+    key = "c",
+    action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+  },
+  {
+    mods = "LEADER",
+    key = "n",
+    action = wezterm.action.ActivateTabRelative(1),
+  },
+  {
+    mods = "LEADER",
+    key = "p",
+    action = wezterm.action.ActivateTabRelative(-1),
+  },
+  {
+    mods = "LEADER",
+    key = "[",
+    action = wezterm.action.ActivateCopyMode,
+  },
+  {
+    mods = "LEADER",
+    key = "Space",
+    action = wezterm.action.ActivateCopyMode,
+  },
+  -- move between split panes
+  split_nav("move", "h"),
+  split_nav("move", "j"),
+  split_nav("move", "k"),
+  split_nav("move", "l"),
+  -- resize panes
+  split_nav("resize", "h"),
+  split_nav("resize", "j"),
+  split_nav("resize", "k"),
+  split_nav("resize", "l"),
+}
+
+for i = 0, 9 do
+  table.insert(config.keys, {
+    mods = "LEADER",
+    key = tostring(i),
+    action = wezterm.action.ActivateTab(i - 1),
+  })
+end
 
 -- and finally, return the configuration to wezterm
 return config
