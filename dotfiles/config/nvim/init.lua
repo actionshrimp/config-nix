8b6636b5410b61766b8fd50490f6896618cd27ad
Dave Aitken <dave.aitken@gmail.com>
2025-08-27 21:25:00 +0100

    simplify pane resize

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -38,10 +38,6 @@ local lazy_spec = {
     "mrjones2014/smart-splits.nvim",
     config = function()
       require("smart-splits").setup({})
-      vim.keymap.set("n", "<A-h>", require("smart-splits").resize_left)
-      vim.keymap.set("n", "<A-j>", require("smart-splits").resize_down)
-      vim.keymap.set("n", "<A-k>", require("smart-splits").resize_up)
-      vim.keymap.set("n", "<A-l>", require("smart-splits").resize_right)
       -- moving between splits
       vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
       vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
--- dotfiles/wezterm.lua
+++ dotfiles/wezterm.lua
@@ -40,22 +40,18 @@ local direction_keys = {
   l = "Right",
 }
 
-local function split_nav(resize_or_move, key)
+local function split_nav(key)
   return {
     key = key,
-    mods = resize_or_move == "resize" and "META" or "CTRL",
+    mods = "CTRL",
     action = wezterm.action_callback(function(win, pane)
       if is_vim(pane) then
         -- pass the keys through to vim/nvim
         win:perform_action({
-          SendKey = { key = key, mods = (resize_or_move == "resize" and "META") or "CTRL" },
+          SendKey = { key = key, mods = "CTRL" },
         }, pane)
       else
-        if resize_or_move == "resize" then
-          win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
-        else
-          win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
-        end
+        win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
       end
     end),
   }
@@ -120,17 +116,32 @@ config.keys = {
     key = "a",
     action = wezterm.action.ShowLauncher,
   },
+  {
+    mods = "LEADER",
+    key = "h",
+    action = wezterm.action.AdjustPaneSize({ "Left", 10 }),
+  },
+  {
+    mods = "LEADER",
+    key = "l",
+    action = wezterm.action.AdjustPaneSize({ "Right", 10 }),
+  },
+  {
+    mods = "LEADER",
+    key = "j",
+    action = wezterm.action.AdjustPaneSize({ "Down", 5 }),
+  },
+  {
+    mods = "LEADER",
+    key = "k",
+    action = wezterm.action.AdjustPaneSize({ "Up", 5 }),
+  },
 
   -- move between split panes
-  split_nav("move", "h"),
-  split_nav("move", "j"),
-  split_nav("move", "k"),
-  split_nav("move", "l"),
-  -- resize panes
-  split_nav("resize", "h"),
-  split_nav("resize", "j"),
-  split_nav("resize", "k"),
-  split_nav("resize", "l"),
+  split_nav("h"),
+  split_nav("j"),
+  split_nav("k"),
+  split_nav("l"),
 }
 
 for i = 0, 9 do
