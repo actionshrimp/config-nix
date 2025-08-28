bec89329199e1d777235176c103c830fd6133366
Dave Aitken <dave.aitken@gmail.com>
2025-08-28 07:31:59 +0100

    try snacks dashboard

--- dotfiles/config/nvim/lua/my-dashboard.lua
+++ dotfiles/config/nvim/lua/my-dashboard.lua
@@ -1,32 +1,6 @@
 local M = {}
-M.plugins = function()
-  return {
-    {
-      "nvimdev/dashboard-nvim",
-      event = "VimEnter",
-      config = function()
-        local s = require("snacks")
-        require("dashboard").setup({
-          theme = "hyper",
-          config = {
-            week_header = {
-              enable = true,
-            },
-            shortcut = {
-              { desc = "󰊳 Update", group = "@property", action = "Lazy update", key = "u" },
-            },
-            project = {
-              enable = true,
-              limit = 8,
-              action = function(path)
-                s.picker.files({ cwd = path })
-              end,
-            },
-          },
-        })
-      end,
-      dependencies = { { "nvim-tree/nvim-web-devicons" } },
-    },
-  }
+M.init = function()
+  local s = require("snacks")
+  s.dashboard.setup({})
 end
 return M
