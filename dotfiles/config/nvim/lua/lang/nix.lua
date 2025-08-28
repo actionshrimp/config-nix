27a10d0edf12c816b7cc0716630b0a9903674fce
Dave Aitken <dave.aitken@gmail.com>
2025-08-28 08:57:43 +0100

    tidy: nixd setup moved

--- dotfiles/config/nvim/lua/lang/nix.lua
+++ dotfiles/config/nvim/lua/lang/nix.lua
@@ -26,37 +26,5 @@ M.init = function()
       },
     },
   })
-  -- vim.api.nvim_create_autocmd("FileType", {
-  --   pattern = "nix",
-  --   callback = function(ev)
-  --     vim.lsp.start({
-  --       name = "nixd",
-  --       cmd = { "nixd" },
-  --       root_dir = vim.fs.root(ev.buf, { "flake.nix" }),
-  --       settings = {
-  --         timeout = 2000,
-  --         nixd = {
-  --           nixpkgs = {
-  --             -- expr = "import <nixpkgs> {}",
-  --             -- # note this only completes on a with pkgs; [ .. ] block
-  --             -- # see https://github.com/nix-community/nixd/issues/492
-  --             expr = 'import (builtins.getFlake ("git+file://" + toString ./.)).inputs.nixpkgs { }',
-  --           },
-  --           options = {
-  --             nixos = {
-  --               expr = '(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations."baracus-hyperv".options',
-  --             },
-  --             nix_darwin = {
-  --               expr = '(builtins.getFlake ("git+file://" + toString ./.)).darwinConfigurations."marco".options',
-  --             },
-  --             home_manager = {
-  --               expr = '(builtins.getFlake ("git+file://" + toString ./.)).homeConfigurations."marco".options',
-  --             },
-  --           },
-  --         },
-  --       },
-  --     })
-  --   end,
-  -- })
 end
 return M
