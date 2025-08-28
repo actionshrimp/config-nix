2300abfc5433fea9264097bd82c3af70c1a4e7bc
Dave Aitken <dave.aitken@gmail.com>
2025-08-28 20:19:13 +0100

    simpler snacks/dashboard

--- dotfiles/config/nvim/lua/my-dashboard.lua
+++ dotfiles/config/nvim/lua/my-dashboard.lua
@@ -1,6 +1,25 @@
-local M = {}
-M.init = function()
-  local s = require("snacks")
-  s.dashboard.setup({})
-end
+local M = {
+  sections = {
+    { section = "header" },
+    { section = "keys", gap = 1, padding = 1 },
+    { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+    { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+    {
+      pane = 2,
+      icon = " ",
+      title = "Git Status",
+      section = "terminal",
+      enabled = function()
+        return Snacks.git.get_root() ~= nil
+      end,
+      cmd = "git status --short --branch --renames",
+      height = 5,
+      padding = 1,
+      ttl = 5 * 60,
+      indent = 3,
+    },
+    { section = "startup" },
+  },
+}
+
 return M
