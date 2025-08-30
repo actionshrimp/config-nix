6483291348642f71e16b935f00c48668b880985d
Dave Aitken <dave.aitken@gmail.com>
2025-08-30 08:32:00 +0100

    dashboard updates

--- dotfiles/config/nvim/lua/my-dashboard.lua
+++ dotfiles/config/nvim/lua/my-dashboard.lua
@@ -1,11 +1,44 @@
 local M = {
   sections = {
     { section = "header" },
-    { section = "keys", gap = 1, padding = 1 },
-    { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-    { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+    -- { section = "keys", gap = 1, padding = 1 },
+    --
     {
-      pane = 2,
+      icon = " ",
+      desc = "Config",
+      padding = 1,
+      key = "c",
+      action = function()
+        require("snacks").picker.files({
+          dirs = { "~/config-nix" },
+        })
+      end,
+    },
+    {
+      icon = " ",
+      desc = "GN (web)",
+      padding = 1,
+      key = "w",
+      action = function()
+        require("snacks").picker.files({
+          dirs = { "~/dev/gn/goodnotes-5/CrossplatformWeb" },
+        })
+      end,
+    },
+    {
+      icon = " ",
+      desc = "GN (root)",
+      padding = 1,
+      key = "r",
+      action = function()
+        require("snacks").picker.files({
+          dirs = { "~/dev/gn/goodnotes-5" },
+        })
+      end,
+    },
+    { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+    { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+    {
       icon = " ",
       title = "Git Status",
       section = "terminal",
