80163a44107dbe5b4f5fb7857c6262f8d1a2bd23
Dave Aitken <dave.aitken@gmail.com>
2025-12-15 09:30:27 +0000

    add sidekick config and keys

--- dotfiles/config/nvim/lua/my-llm.lua
+++ dotfiles/config/nvim/lua/my-llm.lua
@@ -2,6 +2,113 @@ local M = {}
 M.plugins = function()
   return {
     {
+      {
+        "folke/sidekick.nvim",
+        lazy = false,
+        dependencies = {
+          "zbirenbaum/copilot.vim",
+          "copilotlsp-nvim/copilot-lsp",
+        },
+        config = function(args)
+          vim.lsp.enable("copilot")
+          vim.lsp.config("copilot", {})
+          require("sidekick").setup(args.opts)
+        end,
+        opts = {
+          -- add any options here
+          cli = {
+            mux = {
+              backend = "zellij",
+              enabled = true,
+              create = "split",
+            },
+            win = {
+              keys = {
+                buffers = { "<c-n>", "buffers", mode = "nt", desc = "open buffer picker" },
+                files = { "<c-f>", "files", mode = "nt", desc = "open file picker" },
+                hide_n = { "q", "hide", mode = "n", desc = "hide the terminal window" },
+                hide_ctrl_q = { "<c-q>", "hide", mode = "n", desc = "hide the terminal window" },
+                hide_ctrl_dot = { "<c-.>", "hide", mode = "nt", desc = "hide the terminal window" },
+                hide_ctrl_z = { "<c-z>", "hide", mode = "nt", desc = "hide the terminal window" },
+                prompt = { "<c-p>", "prompt", mode = "t", desc = "insert prompt or context" },
+                stopinsert = { "<c-q>", "stopinsert", mode = "t", desc = "enter normal mode" },
+                -- Navigate windows in terminal mode. Only active when:
+                -- * layout is not "float"
+                -- * there is another window in the direction
+                -- With the default layout of "right", only `<c-h>` will be mapped
+                nav_left = { "<c-h>", "nav_left", expr = true, desc = "navigate to the left window" },
+                nav_down = { "<c-j>", "nav_down", expr = true, desc = "navigate to the below window" },
+                nav_up = { "<c-k>", "nav_up", expr = true, desc = "navigate to the above window" },
+                nav_right = { "<c-l>", "nav_right", expr = true, desc = "navigate to the right window" },
+              },
+            },
+          },
+        },
+        keys = {
+          {
+            "<tab>",
+            function()
+              -- if there is a next edit, jump to it, otherwise apply it if any
+              if not require("sidekick").nes_jump_or_apply() then
+                return "<Tab>" -- fallback to normal tab
+              end
+            end,
+            expr = true,
+            desc = "Goto/Apply Next Edit Suggestions ",
+            mode = { "n" },
+          },
+          {
+            "<S-tab>",
+            function()
+              -- clear the next edit suggestions
+              require("sidekick.nes").toggle()
+            end,
+            expr = true,
+            desc = "Clear Next Edit Suggestions",
+            mode = { "n" },
+          },
+          --   {
+          --     "<c-.>",
+          --     function()
+          --       require("sidekick.cli").toggle()
+          --     end,
+          --     desc = "Sidekick Toggle",
+          --     mode = { "n", "t", "i", "x" },
+          --   },
+          --   {
+          --     "<leader>aa",
+          --     function()
+          --       require("sidekick.cli").toggle()
+          --     end,
+          --     desc = "Sidekick Toggle CLI",
+          --   },
+          --   {
+          --     "<leader>at",
+          --     function()
+          --       require("sidekick.cli").send({ msg = "{this}" })
+          --     end,
+          --     mode = { "x", "n" },
+          --     desc = "Send This",
+          --   },
+          --   {
+          --     "<leader>af",
+          --     function()
+          --       require("sidekick.cli").send({ msg = "{file}" })
+          --     end,
+          --     desc = "Send File",
+          --   },
+          --   {
+          --     "<leader>ap",
+          --     function()
+          --       require("sidekick.cli").prompt()
+          --     end,
+          --     mode = { "n", "x" },
+          --     desc = "Sidekick Select Prompt",
+          --   },
+        },
+      },
+    },
+    {
       "yetone/avante.nvim",
       event = "VeryLazy",
       lazy = false,
