7974932ba475c47f67b79ed0efbf0e492c903d8c
Dave Aitken <dave.aitken@gmail.com>
2026-01-19 15:58:54 +0000

    fix: split binds in neovim terminal

--- dotfiles/config/nvim/lua/my-terminal.lua
+++ dotfiles/config/nvim/lua/my-terminal.lua
@@ -17,10 +17,11 @@ M.init = function()
   function _G.set_terminal_keymaps()
     local opts = { buffer = 0 }
     vim.keymap.set("t", [[<C-\><C-\>]], [[<C-\><C-n>]], opts)
-    vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
-    vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
-    vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
-    vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+    -- Use smart-splits for Zellij-aware navigation
+    vim.keymap.set("t", "<C-h>", require("smart-splits").move_cursor_left, opts)
+    vim.keymap.set("t", "<C-j>", require("smart-splits").move_cursor_down, opts)
+    vim.keymap.set("t", "<C-k>", require("smart-splits").move_cursor_up, opts)
+    vim.keymap.set("t", "<C-l>", require("smart-splits").move_cursor_right, opts)
   end
 
   -- if you only want these mappings for toggle term use term://*toggleterm#* instead
